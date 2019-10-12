937a88fcf660bb8eac0196cc31efec4c0e827aa3
Luis Adorno <luisadorno@Luiss-MacBook-Pro.local>
2019-10-12 16:14:05 -0400

    Add Hero Service dependency injection and fix hero details.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../Hero'
-import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  onSelect(hero: Hero): void{
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
-
-  ngOnInit() {
+  getHeroes(): void{
+    this.heroes = this.heroService.getHeroes();
   }
-
 }
