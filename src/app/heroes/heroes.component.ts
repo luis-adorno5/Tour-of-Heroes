246fb42e60c10f3dc311ade96bf2d5308f5fead0
Luis Adorno <luisadorno@Luiss-MacBook-Pro.local>
2019-10-12 14:21:50 -0400

    Add hero detail under hero list.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,10 +10,11 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
